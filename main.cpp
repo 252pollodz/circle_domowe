e1abdf47cbefd528675716d5bc03d53d8a8a56cc
252pollodz <252787@edu.p.lodz.pl>
2025-06-08 20:37:22 +0200

    Dodanie-pliku

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Point.h"
+using namespace std;
+
+int main()
+{
+    Point points[10] = {
+        Point("P1", 0, 0),
+        Point("P2", 10, 20),
+        Point("P3", 30, 40),
+        Point("P4", 50, 60),
+        Point("P5", 70, 80),
+        Point(),
+        Point(),
+        Point("P8",15,25)
+      };
+
+    cout << "Istnieje " << Point::numberOfPoints() << " punktow."<< endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        points[i].show();
+    }
+
+
+    {
+        Point p1, p2, p3;
+        cout << endl;
+        cout << "Teraz istnieje " << Point::numberOfPoints() << " punktow." << endl;
+    }
+
+    cout << "A teraz istnieje " << Point::numberOfPoints() << " punktow." << endl;
+
+}
